ffe93e0129b23cab9e094b7f81f2faeba44963e1
Eric <eric.keith@flatironschool.com>
2023-02-01 17:26:28 -0600

    commit tree 36b34d33b02aa3efdafd37e038667b4e97c65e53

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat () {
+    let cats2 = [...cats, "Broom"]
+    return cats2;
+}
+
+function prependCat () {
+    let cats3 = ["Arnold", ...cats]
+    return cats3;
+}
+
+function removeLastCat() {
+    let cats4 = [...cats.slice(0,2)]
+    return cats4
+}
+
+function removeFirstCat() {
+    let cats5 = [...cats.slice(1,3)]
+    return cats5
+}
